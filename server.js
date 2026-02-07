c20d0a180a95bf03880492621befc5dfed0380ac
Chavala Sandesh <sandesh.254t@gmail.com>
2026-02-07 22:21:54 +0530

    Serve Trail.html on root route

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Trail.html"));
+});
+
  
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
